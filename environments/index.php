76ccb934d5d2692be27e6bada47a10871f703c6a
hubeiwei <hubeiwei@hotmail.com>
2017-06-08 12:56:16 +0800

    整理 .gitignore 相关

--- environments/index.php
+++ environments/index.php
@@ -45,20 +45,4 @@ return [
             'frontend/config/main-local.php',
         ],
     ],
-    'Production' => [
-        'path' => 'prod',
-        'setWritable' => [
-            'backend/runtime',
-            'backend/web/assets',
-            'frontend/runtime',
-            'frontend/web/assets',
-        ],
-        'setExecutable' => [
-            'yii',
-        ],
-        'setCookieValidationKey' => [
-            'backend/config/main-local.php',
-            'frontend/config/main-local.php',
-        ],
-    ],
 ];
