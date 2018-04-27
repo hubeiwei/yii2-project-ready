7cf516312b9de001f5a0da35d6b1e1e27ddbd1cd
hubeiwei <hubeiwei@hotmail.com>
2018-04-27 17:53:48 +0800

    添加 frontend 应用

---
+++ frontend/controllers/SiteController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace frontend\controllers;
+
+use yii\web\Controller;
+
+/**
+ * Site controller
+ */
+class SiteController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+}
